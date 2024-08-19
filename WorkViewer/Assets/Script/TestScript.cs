7c083f52fbe8db4fd00e930d64539731f6904f8f
Yeo <yeodaehyeon@gmail.com>
2024-08-19 22:35:54 +0900

    Model 회전 테스트 진행

--- WorkViewer/Assets/Script/TestScript.cs
+++ WorkViewer/Assets/Script/TestScript.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform model;
 
-    // Update is called once per frame
+    private Vector2 touchStartPosition;
+    private Quaternion startRotation;
+    float rotationAmount = 0.1f;
+
     void Update()
     {
-        
+        if (model == null)return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartRotation();
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            RotateModel();
+        }
+    }
+
+    private void StartRotation()
+    {
+        if (IsPointerOverUI()) return;
+
+        touchStartPosition = Input.mousePosition;
+        startRotation = model.rotation;
+    }
+
+    private void RotateModel()
+    {
+        Vector2 touchDelta = (Vector2)Input.mousePosition - touchStartPosition;
+
+        float rotationX = touchDelta.y * rotationAmount;
+        float rotationY = -touchDelta.x * rotationAmount;
+
+        model.rotation = startRotation;
+        model.Rotate(Vector3.right, rotationX, Space.World);  
+        model.Rotate(Vector3.up, rotationY, Space.World);    
+    }
+
+    private bool IsPointerOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
     }
 }
